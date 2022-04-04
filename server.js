c902ea81a68866aa2308be88462df572642f2351
David Reyes Santoyo <deiv@Deivs-MBP.local>
2022-04-03 23:24:22 -0700

    helpers added to pluralize, format date, and optimize url lengths

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const routes = require('./controllers');
+const helpers = require('./utils/helpers');
 const sequelize = require('./config/connection');
 const path = require('path');
 
 const exphbs = require('express-handlebars');
-const hbs = exphbs.create({});
+const hbs = exphbs.create({ helpers });
 
 
 
